5d21e444521b57ab65fcf1ec75b1c15aa4db9c5f
M3832 <markus3832@gmail.com>
2017-02-16 12:37:53 +0100

    Initierade vår Express-app
    La till de absolut mest grundläggande sakerna för att få vår Express-app
    att starta, lade även till ett par moduler vi kommer använda.

---
+++ server.js
@@ -0,0 +1,41 @@
+//Importera alla moduler
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyparser = require('body-parser');
+const app = express();
+const mysql = require('mysql');
+const uuid = require('uuid/v1');
+
+//Säg till appen (express) var den hittar statiska filer, så som javascript-filer och css-filer
+app.use(express.static(path.join(__dirname, '/public')));
+
+//Säg till appen vilken view engine vi använder, handlebars, och berätta i vilken mapp våra .hbs-filer
+//kommer ligga
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+//Säg till appen hur vi vill parsa inkommande data som skickas från en sida till en annan (via t ex formulär)
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use(bodyparser.json());
+
+//Säg till vilken port den ska lyssna på.
+/*
+För att köra denna server, kör "node server.js" när du står i den här katalogen.
+Öppna sedan localhost:3000 i en webbläsare.
+ */
+app.listen(3000);
+
+
+//APP REQUESTS
+
+app.get('/', function(req, res, next){
+    res.send('Välkommen till vårt testverktyg!');
+});
