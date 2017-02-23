569c4de73c63e5076c92604dddcb314b528c5f50
SofHay <sofhay@live.se>
2017-02-23 09:19:46 +0100

    uppdatering insert.js

--- NodeModules/insert.js
+++ NodeModules/insert.js
@@ -16,6 +16,7 @@ addUser('Markus', 'Gustafsson', 'markus3832@gmail.com', 'jupp', 'teacher');
 addUser('Sofia', 'Hayoti', 'sofhay@live.se','puss','student' );
 
 function addUser(ufirstName,ulastName, umail, upassword,urole) {
+<<<<<<< Updated upstream
     var newUser ={
         firstname: ufirstName,
         lastname: ulastName,
@@ -31,4 +32,30 @@ function addUser(ufirstName,ulastName, umail, upassword,urole) {
        console.log("Bra jobbat hayoti");
        return true;
        })
+=======
+    if(!validateEmail(umail)){
+        return false;
+    }
+    else {
+
+        var newUser = {
+            firstname: ufirstName,
+            lastname: ulastName,
+            mail: umail,
+            password: upassword,
+            role: urole
+        };
+        var query = connection.query('INSERT INTO User set ?', newUser, function (err, resilt) {
+            if (err) {
+                return false;
+            }
+            return true;
+        });
+    }
+>>>>>>> Stashed changes
 };
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
