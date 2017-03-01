1a638fb2328d14fd2a80eb9c8660faa72c45fd66
SofHay <sofhay@live.se>
2017-03-01 13:08:07 +0100

    validering

--- public/js/saveGroup.js
+++ public/js/saveGroup.js
@@ -6,27 +6,44 @@
 $(function(){
     $("#createGroup").click(function (event) {
         event.preventDefault();
-        var ids = [];
-        $('ul#groupList li').each(function(){//Loops through the amount of users in the group
 
-            ids.push($(this).attr('id')); //Adds each user in an array
-        });
-        //Create object groupData and save groupData in Ajax
-        var groupData = {};
-        console.log($('#testName').val());
-        groupData.title = $('#testName').val();
-        groupData.ids = ids;
+        var name = $('#testName').val();
+        console.log(name);
+        if(name == '') {
 
-        $.ajax({
-            url: '/group',
-            method: 'post',
-            data: groupData,
-            success: function(){
-                console.log('Hurra?');
-                location.replace('/');
-            }});
+            document.getElementById('missingTitle').className = 'alert alert-danger';
+            document.getElementById('missingTitle').textContent = 'Ange namn på gruppen';
+            document.getElementById('inputError').className = 'form-group  has-error' ;
+        }
+        else if($('ul#groupList li').length==0){
+            document.getElementById('missingMembers').className = 'alert alert-danger';
+            document.getElementById('missingMembers').textContent = 'Gruppen saknar medlemmar';
 
+        }
+        else
+            {
+            var ids = [];
+            $('ul#groupList li').each(function () {//Loops through the amount of users in the group
 
+                ids.push($(this).attr('id')); //Adds each user in an array
+            });
+            //Create object groupData and save groupData in Ajax
+            var groupData = {};
+            console.log($('#testName').val());
+            groupData.title = $('#testName').val();
+            groupData.ids = ids;
+
+            $.ajax({
+                url: '/group',
+                method: 'post',
+                data: groupData,
+                success: function () {
+                    console.log('Hurra?');
+                    location.replace('/');
+                }
+            });
+
+        }
     });
 });
 
